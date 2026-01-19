c1f71b872dd3c5d46317e5ea1e0331cf9b299d27
chendonghui <yzucdh@163.com>
2026-01-19 18:20:53 +0800

    modify failed

--- test/ProxyAuction.ts
+++ test/ProxyAuction.ts
@@ -1,8 +1,8 @@
 import { expect } from "chai";
 import hre from "hardhat";
 
-import NftAuctionModel from "../ignition/modules/ProxyModule.js";
-import UpgradeModule from "../ignition/modules/UpgradeModule.js";
+import nftAuctionModel from "../ignition/modules/ProxyModule.js";
+import nftAuctionV2Module from "../ignition/modules/UpgradeModule.js";
 
 const { ethers, ignition } = await hre.network.connect();
 
@@ -10,7 +10,7 @@ describe("Proxy", function () {
     describe("Proxy interaction", function () {
         it("Should be usable via proxy", async function () {
             const [, otherAccount] = await ethers.getSigners();
-            const { nftAuction } = await ignition.deploy(NftAuctionModel.nftAuctionModule);
+            const { nftAuction } = await ignition.deploy(nftAuctionModel);
 
             const version = await nftAuction.getVersion({ account: otherAccount.address });
             expect(version).to.equal("1.0.0");
@@ -20,7 +20,7 @@ describe("Proxy", function () {
     describe("Upgrading", function () {
         it("Should have upgraded the proxy to NftAuctionV2", async function () {
             const [, otherAccount] = await ethers.getSigners();
-            const { nftAuctionV2 } = await ignition.deploy(UpgradeModule.nftAuctionV2Module);
+            const { nftAuctionV2 } = await ignition.deploy(nftAuctionV2Module);
 
             const version = await nftAuctionV2.getVersion({ account: otherAccount.address });
             expect(version).to.equal("2.0.0");
